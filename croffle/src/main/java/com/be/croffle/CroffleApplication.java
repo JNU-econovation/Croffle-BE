fb620eaa9b387113abaed245f3c21e9e8a09badc
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-08 17:34:34 +0900

    feat : 프로젝트 초기세팅

---
+++ croffle/src/main/java/com/be/croffle/CroffleApplication.java
@@ -0,0 +1,13 @@
+package com.be.croffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroffleApplication.class, args);
+	}
+
+}
---
+++ croffle/src/test/java/com/be/croffle/CroffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.croffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
