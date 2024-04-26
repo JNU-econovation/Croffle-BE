3553a46fe7592e0383fc8b84a391994379dfebcb
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-26 21:32:27 +0900

    feat : 응답형식 작성

---
+++ croffle/src/main/java/com/be/croffle/common/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.be.croffle.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.Serializable;
+
+@Getter
+public class ApiResponse<B> extends ResponseEntity<B> {
+
+    public ApiResponse(B body, HttpStatus status) {
+        super(body, status);
+    }
+
+    public ApiResponse(B body, MediaType mediaType, HttpStatus status) {
+        super(body, status);
+        this.getHeaders().setContentType(mediaType);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class CustomBody<D> implements Serializable {
+        private Boolean success;
+        private D response;
+        private Error error;
+    }
+}
