e82d734db2b716bc878eb88ec1e1d8b18ca0b87a
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-26 22:07:19 +0900

    feat : MusicGenService작성

---
+++ croffle/src/main/java/com/be/croffle/service/MusicGenService.java
@@ -0,0 +1,120 @@
+package com.be.croffle.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.be.croffle.music.Music;
+import com.be.croffle.music.MusicJpaRepository;
+import com.be.croffle.music.dto.MusicGenRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MusicGenService {
+
+    private final AmazonS3Client amazonS3Client;
+    private final MusicJpaRepository musicJpaRepository;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private String workingDir = System.getProperty("user.dir");
+
+    //TODO : 환경변수화
+
+    //  @Value("${python.script.path}")
+    private String scriptPath= workingDir + "/src/main/resources/script.py";
+
+    //  @Value("${python.executable.path}")
+    private String pythonExecutablePath= workingDir + "/myvenv/bin/python3";
+
+    public String generateMusic(MusicGenRequest reqDto){
+        JSONObject jsonObject = createJsonFromRequest(reqDto);
+        executePythonScript(jsonObject.toString());
+
+        String musicUrl = uploadFileToS3();
+        musicJpaRepository.save(Music
+                .builder()
+                .musicUrl(musicUrl)
+                .build());
+
+        return musicUrl;
+    }
+
+    private JSONObject createJsonFromRequest(MusicGenRequest reqDto) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("speed", reqDto.speed());
+        jsonObject.put("mood", reqDto.mood());
+        jsonObject.put("loc", reqDto.loc());
+        return jsonObject;
+    }
+
+    private void executePythonScript(String jsonInput){
+        ProcessBuilder processBuilder = new ProcessBuilder(pythonExecutablePath, scriptPath);
+        processBuilder.redirectErrorStream(true);
+
+        try{
+            Process process = processBuilder.start();
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream(), StandardCharsets.UTF_8))) {
+                writer.write(jsonInput);
+                writer.flush();
+            }
+
+            // 성공적으로 실행된 경우, .wav 파일 경로를 반환
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            logProcessOutput(reader);
+            waitForProcessAndLogExitCode(process);
+
+        } catch (IOException | InterruptedException e) {
+           e.printStackTrace();
+       }
+    }
+
+    private void logProcessOutput(BufferedReader reader) throws IOException {
+        String output = reader.lines().reduce("", (acc, line) -> acc + "\n" + line);
+        log.info("Python script output: {}", output);
+    }
+
+    private void waitForProcessAndLogExitCode(Process process) throws InterruptedException {
+        int exitCode = process.waitFor();
+        log.info("Python script exited with code {}", exitCode);
+    }
+
+    private String uploadFileToS3() {
+        // AWS S3 업로드 로직 구현
+        Long id = 1L;
+        //TODO : 음악 확장자 확인
+        File uploadFile =  new File(System.getProperty("user.dir") + "/musics/" + "music" + id +".wav");
+
+        String fileName = "music" + "/" + uploadFile.getName(); // S3에 저장된 파일 이름
+        System.out.println("fileName = " + uploadFile);
+        String uploadMusicUrl = putS3(uploadFile, fileName); // S3로 업로드
+        log.info("uploadImageUrl = " + uploadMusicUrl);
+        removeNewFile(uploadFile);
+
+        // 업로드된 음악 url 제공
+        return uploadMusicUrl;
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+}
