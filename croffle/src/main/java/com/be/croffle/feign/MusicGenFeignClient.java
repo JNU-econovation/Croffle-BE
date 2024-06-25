48408bdd91389dbe28303f1a3ea5f67906822e51
MinjuKwak01 <mkwak1125@gmail.com>
2024-06-25 17:33:02 +0900

    fix : rename response dto

--- croffle/src/main/java/com/be/croffle/feign/MusicGenFeignClient.java
+++ croffle/src/main/java/com/be/croffle/feign/MusicGenFeignClient.java
@@ -1,7 +1,7 @@
 package com.be.croffle.feign;
 
-import com.be.croffle.music.dto.GeneratedUrl;
 import com.be.croffle.music.dto.MusicGenRequest;
+import com.be.croffle.music.dto.ServerResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
         configuration = FeignClientConfig.class)
 public interface MusicGenFeignClient {
     @PostMapping("/generate_audio")
-    GeneratedUrl generateMusic(@RequestBody MusicGenRequest reqDto);
+    ServerResponse generateMusic(@RequestBody MusicGenRequest reqDto);
 
 }
