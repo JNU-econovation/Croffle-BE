95db95637125713d328aeaddf34b0d5858540535
MinjuKwak01 <mkwak1125@gmail.com>
2024-06-29 23:25:22 +0900

    feat : add MyMusicJpaRepository

--- croffle/src/main/java/com/be/croffle/music/mymusic/MyMusicJpaRepository.java
+++ croffle/src/main/java/com/be/croffle/music/mymusic/MyMusicJpaRepository.java
@@ -1,5 +1,6 @@
-package com.be.croffle.music;
+package com.be.croffle.music.mymusic;
 
+import com.be.croffle.music.mymusic.Mymusic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MyMusicJpaRepository extends JpaRepository<Mymusic, Long> {
