38ea09c74d7e72470e0e0d225147c61e033d09f4
MinjuKwak01 <mkwak1125@gmail.com>
2024-06-29 23:19:18 +0900

    fix : add custom filter, functions

--- croffle/src/main/java/com/be/croffle/common/security/SecurityConfig.java
+++ croffle/src/main/java/com/be/croffle/common/security/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.be.croffle.common.security;
 
+import com.be.croffle.common.error.ErrorMessage;
+import com.be.croffle.common.error.ForbiddenException;
+import com.be.croffle.common.error.UnAuthorizedException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,7 +23,19 @@ import java.util.Arrays;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TokenGenerator tokenGenerator;
+
+    public class SecurityFilterManagerImpl extends AbstractHttpConfigurer<SecurityFilterManagerImpl, HttpSecurity> {
+        public void configure(HttpSecurity builder) throws Exception {
+            final AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder.addFilter(new JwtAuthenticationFilter(authenticationManager, tokenGenerator))
+                    .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
+            super.configure(builder);
+        }
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, @Autowired @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) throws Exception {
@@ -42,31 +58,46 @@ public class SecurityConfig {
 
         //커스텀 필터 등록
         //http.apply(new SecurityFilterManagerImpl()); <- deprecated
-        // http.with(new SecurityFilterManagerImpl(), securityFilterManager -> {
-        //  });
+        http.with(new SecurityFilterManagerImpl(), securityFilterManager -> {
+        });
 
-        /*
+
         // 인증 실패 처리
         http.exceptionHandling(handling ->
                 handling.authenticationEntryPoint(((request, response, authException) -> {
-                    resolver.resolveException(request, response, null, new UnAuthorizedException(ErrorMessage.UNAUTHORIZED_ERROR));
+                    resolver.resolveException(request, response, null, new UnAuthorizedException(ErrorMessage.UNAUTHORIZED));
                 })));
-         */
 
-        /*
+
+
         // 권한 실패 처리
         http.exceptionHandling(handling ->
                 handling.accessDeniedHandler(((request, response, accessDeniedException) -> {
-                    resolver.resolveException(request, response, null, new ForbiddenException(ErrorMessage.FORBIDDEN_ERROR));
+                    resolver.resolveException(request, response, null, new ForbiddenException(ErrorMessage.FORBIDDEN));
                 })));
-         */
+
+
 
         //authorization setting
+        /*
         http.authorizeHttpRequests(authorize ->
                 authorize
+                        .requestMatchers(new AntPathRequestMatcher("/api/myplaylist", "GET")).authenticated()
                         .requestMatchers(new AntPathRequestMatcher("/api/playlist", "GET")).authenticated()
                         .anyRequest().permitAll()
         );
+
+         */
+        /*
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/generate-music").permitAll()
+                .requestMatchers("/api/playlist").permitAll()
+                .anyRequest().authenticated()
+        );
+
+         */
+
+
 
         return http.build();
 
