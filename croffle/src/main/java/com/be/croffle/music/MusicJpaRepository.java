3b0b7319f5f0540c9a073484dcf7adc10cda561c
MinjuKwak01 <mkwak1125@gmail.com>
2024-07-12 23:02:44 +0900

    feat : order music by like number

--- croffle/src/main/java/com/be/croffle/music/MusicJpaRepository.java
+++ croffle/src/main/java/com/be/croffle/music/MusicJpaRepository.java
@@ -1,6 +1,12 @@
 package com.be.croffle.music;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MusicJpaRepository extends JpaRepository<Music, Long> {
+
+    @Query("SELECT m FROM Music m LEFT JOIN Like l ON m.id = l.music.id GROUP BY m.id ORDER BY COUNT(l.id) DESC")
+    Page<Music> findAllOrderByLikeCountDesc(Pageable pageable);
 }
