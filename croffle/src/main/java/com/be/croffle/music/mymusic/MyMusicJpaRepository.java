1188b0354bf86f525cd4469750eb864c1a89f479
MinjuKwak01 <mkwak1125@gmail.com>
2024-07-14 19:49:46 +0900

    feat : findAllByMemberId method in MyMusicJpaRepository

--- croffle/src/main/java/com/be/croffle/music/mymusic/MyMusicJpaRepository.java
+++ croffle/src/main/java/com/be/croffle/music/mymusic/MyMusicJpaRepository.java
@@ -1,7 +1,13 @@
 package com.be.croffle.music.mymusic;
 
 import com.be.croffle.music.mymusic.Mymusic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MyMusicJpaRepository extends JpaRepository<Mymusic, Long> {
+
+    Page<Mymusic> findAllByMemberId(Pageable pageable, Long memberId);
 }
